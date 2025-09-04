a4aae21bfdc0380e54eb2c4883f1aca1edfd83e0
Theshan <theshangeethanjana@gmail.com>
2025-09-04 11:53:44 +0530

    Fix base path and router basename for GitHub Pages

--- JW-Studio/src/main.jsx
+++ JW-Studio/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/JW-studio-system">
 
     <App />
   </BrowserRouter>,
--- JW-Studio/vite.config.js
+++ JW-Studio/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/JW-studio-system/",
   plugins: [tailwindcss(),react()],
 })
 
